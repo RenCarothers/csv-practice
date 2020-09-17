8d276455a8e6a51940d79725cb95656db151f70a
Ren Carothers <rencarothers@gmail.com>
2020-09-16 21:54:40 -0700

    fixed broken csv path in tests, passed first method tests

--- lib/csv_practice.rb
+++ lib/csv_practice.rb
@@ -2,8 +2,29 @@ require 'csv'
 require 'awesome_print'
 
 def get_all_olympic_athletes(filename)
+  # take in a file name (a string with a path)
+  # return an array of Olympic medalists,
+  # each person a hash element within the array,
+  # listing only these keys: id, name, height, team, year, city, sport, event, medal
+  # a test could be: does it output an array? ea index hashes?
+  array_of_hashes = CSV.read(filename, headers: true).map do |row|
+    row.to_h
+  end
 
+  #Sex,Age,Weight,NOC,Year,Season,
+  array_of_hashes.each do |hash|
+    hash.delete("Sex") if hash.include?("Sex")
+    hash.delete("Age") if hash.include?("Age")
+    hash.delete("Weight") if hash.include?("Weight")
+    hash.delete("NOC") if hash.include?("NOC")
+    hash.delete("Games") if hash.include?("Games")
+    hash.delete("Season") if hash.include?("Season")
+
+  end
+  return array_of_hashes
 end
+
+ap get_all_olympic_athletes('../data/athlete_events.CSV')
 
 def total_medals_per_team(olympic_data)
 
--- test/csv_practice_test.rb
+++ test/csv_practice_test.rb
@@ -13,8 +13,8 @@ require_relative '../lib/csv_practice'
 Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
 REQUIRED_OLYMPIAN_FIELDS = %w[ID Name Height Team Year City Sport Event Medal]
-MEDAL_TOTALS_FILENAME = 'data/medal_totals.csv'
-OLYMPIC_DATA_FILENAME = 'data/athlete_events.csv'
+MEDAL_TOTALS_FILENAME = '../data/medal_totals.csv'
+OLYMPIC_DATA_FILENAME = '../data/athlete_events.csv'
 
 describe "CSV and Enumerables Exercise" do
 
